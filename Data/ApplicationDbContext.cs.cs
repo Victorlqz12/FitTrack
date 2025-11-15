506dc02df7c917b474fcddcf3f6c3b21c07c5414
victor alves da silva <victor.alves951@hotmail.com>
2025-11-15 00:13:07 -0300

    Configuração e criação banco de dados

---
+++ Data/ApplicationDbContext.cs.cs
@@ -0,0 +1,18 @@
+﻿using FitTrack.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitTrack.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios => Set<Usuario>();
+        public DbSet<Treino> Treinos => Set<Treino>();
+        public DbSet<Exercicio> Exercicios => Set<Exercicio>();
+        public DbSet<Evolucao> Evolucoes => Set<Evolucao>();
+    }
+}
---
+++ Migrations/20251115031212_InitialCreate.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitTrack.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Evolucoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataRegistro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Peso = table.Column<double>(type: "float", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Evolucoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Evolucoes_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Treinos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NomeTreino = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Treinos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Treinos_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exercicios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeExercicio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Series = table.Column<int>(type: "int", nullable: false),
+                    Repeticoes = table.Column<int>(type: "int", nullable: false),
+                    Carga = table.Column<double>(type: "float", nullable: false),
+                    TreinoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercicios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Exercicios_Treinos_TreinoId",
+                        column: x => x.TreinoId,
+                        principalTable: "Treinos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Evolucoes_UsuarioId",
+                table: "Evolucoes",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exercicios_TreinoId",
+                table: "Exercicios",
+                column: "TreinoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Treinos_UsuarioId",
+                table: "Treinos",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Evolucoes");
+
+            migrationBuilder.DropTable(
+                name: "Exercicios");
+
+            migrationBuilder.DropTable(
+                name: "Treinos");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
